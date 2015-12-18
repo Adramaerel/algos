392b1244af10b78dc184c6d6ada441e7ced53698
sophie <sophiebucaille@gmail.com>
2015-12-18 22:48:11 +0100

    uva

--- uva/Contest_Volumes_(10000...)/Volume_109_(10900-10999)/P10929.java
+++ uva/Contest_Volumes_(10000...)/Volume_109_(10900-10999)/P10929.java
@@ -1,1 +1,27 @@
-public class
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class P10929 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		String ligne;
+		BufferedReader entree = new BufferedReader(new InputStreamReader(System.in));
+		while(!(ligne = entree.readLine()).equals("0")) {
+			ligne = ligne.trim();
+			BigInteger bigint = new BigInteger(ligne);
+			if(bigint.mod(new BigInteger("11")).equals(BigInteger.ZERO)) {
+				System.out.println(ligne + " is a multiple of 11.");
+			} else {
+				System.out.println(ligne + "is not a multiple of 11.");
+			}
+		}
+		
+	}
+
+}
+
--- uva/Contest_Volumes_(10000...)/Volume_111_(11100-11199)/11172.c
+++ uva/Contest_Volumes_(10000...)/Volume_111_(11100-11199)/11172.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
+int main()
+{
 
 	int number_of_inputs, compteur, n, m;
 
