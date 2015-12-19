37013d423ed4209b08e3ce5e333db299bf7cc671
sophie <sophiebucaille@gmail.com>
2015-12-19 18:02:25 +0100

    uva pb 494

---
+++ uva/Problem_Set_Volumes_(100...1999)/Volume_4_(400-499)/P494.java
@@ -0,0 +1,28 @@
+package uva;
+
+import java.util.Scanner;
+public class P494 {
+
+	public static void main (String args[]) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			boolean debutMot = false;
+			int count = 0;
+			char[] mots =  sc.nextLine().toCharArray();
+			for (char lettre : mots) {
+				if(Character.isLetter(lettre)) {
+					if(!debutMot) {
+						count++;
+						debutMot = true;
+					}
+				}
+				else {
+					debutMot= false;
+				}
+			}
+			System.out.println(count);
+		}
+	}
+}
+
