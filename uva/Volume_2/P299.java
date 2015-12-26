6f0dcab4ae4bacca6e8074fefff92f18d022430c
sophie <sophiebucaille@gmail.com>
2015-12-26 12:28:33 +0100

    Uva solution 299 in Java Train Swapping

---
+++ uva/Volume_2/P299.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P299 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Integer casesNb = sc.nextInt();
+		for(int i = 0; i < casesNb; i++) {
+			int swapCount = 0;
+			Integer carriagesNb = sc.nextInt();
+			Integer[] list = new Integer[carriagesNb];
+			for(int j = 0; j < carriagesNb; j++) {
+				list[j] = sc.nextInt();
+			}
+			while(true) {
+				Boolean hasChanged = false;
+				for(int j = 0; j < list.length -1; j++) {
+					if(list[j] > list[j+1]) {
+						int swap = list[j];
+						list[j] = list[j + 1];
+						list[j + 1] = swap;
+						swapCount++;
+						hasChanged = true;
+					}
+				}
+				if(!hasChanged) {
+					break;
+				}
+			}
+			System.out.println("Optimal train swapping takes " + swapCount + " swaps.");
+			
+		}
+		
+	}
+
+}
+
