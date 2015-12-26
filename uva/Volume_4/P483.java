f292e8077a4f4aaaa42e3935ffc5551fbf12937f
sophie <sophiebucaille@gmail.com>
2015-12-26 12:06:51 +0100

    UVA solution 483 in Java Word Scramble

---
+++ uva/Volume_4/P483.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P483 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String ligne;
+		while(sc.hasNext()) {
+			ligne = sc.nextLine();
+			String [] words = ligne.split(" ");
+			for(int i = 0; i < words.length; i++)  {
+				String word = words[i];	
+				for(int j = 0; j < word.length(); j++) {
+					System.out.print(word.charAt(word.length() - j - 1));
+				}
+				if(i < words.length - 1)
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
