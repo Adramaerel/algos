525f68f49ddafaf2209ef92ba3446adb6379f151
sophie <sophiebucaille@gmail.com>
2016-01-01 21:50:16 +0100

    uva 11875 solution brickgame

---
+++ uva/Volume_118/11875.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P11875 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Integer cNb = Integer.parseInt(sc.nextLine());
+		for(int i = 0; i < cNb; i++) {
+			String [] tab = sc.nextLine().split(" ");
+			Integer input [] = new Integer[tab.length - 1];
+			for(int j = 0; j < input.length;j++) {
+				input[j] = Integer.parseInt(tab[j + 1]);
+			}
+			Arrays.sort(input);
+			System.out.printf("Case %d: %d\n", i +1, (int)input[input.length / 2]);
+		}
+	}
+
+}
+
