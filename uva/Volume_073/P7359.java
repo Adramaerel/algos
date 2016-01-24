520a6fac3e2493365713f21198e5739645a1b142
sophie <sophiebucaille@gmail.com>
2016-01-24 15:50:53 +0100

    uva 7359 solution in java :  Sum Kind Of Problem

---
+++ uva/Volume_073/P7359.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class P7359 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Integer casesNb = Integer.parseInt(sc.nextLine());
+		for(int i = 1; i <= casesNb; i++) {
+			String []input = sc.nextLine().split(" " );
+			System.out.printf("%d %d %d %d\n", i, sum_1(Integer.parseInt(input[1])),
+					sum_2(Integer.parseInt(input[1])), sum_3(Integer.parseInt(input[1])));
+			
+		}
+		
+		
+		
+	}
+
+	private static int sum_3(int parseInt) {
+		// TODO Auto-generated method stub
+		return parseInt * (parseInt + 1);
+	}
+
+	private static int sum_2(int parseInt) {
+		// TODO Auto-generated method stub
+		return parseInt * parseInt;
+	}
+
+	private static int sum_1(int parseInt) {
+		// TODO Auto-generated method stub
+		return parseInt * (parseInt + 1) /2;
+	}
+
+}
+
