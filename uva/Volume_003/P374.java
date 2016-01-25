19bb12ed74d44a832bec98d68ea498ace3a83f56
sophie <sophiebucaille@gmail.com>
2016-01-25 18:08:15 +0100

    uva solution 374 in java Big Mod

---
+++ uva/Volume_003/P374.java
@@ -0,0 +1,26 @@
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P374 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		BigInteger b, p, m;
+		
+		while(sc.hasNext()) {
+			
+			b = sc.nextBigInteger();
+			p = sc.nextBigInteger();
+			m = sc.nextBigInteger();
+			
+			System.out.println(b.modPow(p, m));
+			
+		}
+		
+	}
+
+}
+
