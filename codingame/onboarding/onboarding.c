d0c4eadbbd919310affeef426cf715bb10136a0f
sophie <sophiebucaille@gmail.com>
2016-01-25 21:06:03 +0100

    codingame onboarding and Temperatures

---
+++ codingame/onboarding/onboarding.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+
+    // game loop
+    while (1) {
+        char enemy1[257]; // name of enemy 1
+        scanf("%s", enemy1);
+        int dist1; // distance to enemy 1
+        scanf("%d", &dist1);
+        char enemy2[257]; // name of enemy 2
+        scanf("%s", enemy2);
+        int dist2; // distance to enemy 2
+        scanf("%d", &dist2);
+
+        if(dist1 > dist2) {
+            printf("%s\n", enemy2);
+        }
+        else {
+            printf("%s\n", enemy1);
+        }
+    }
+
+    return 0;
+}
