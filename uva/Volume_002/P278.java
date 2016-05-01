8f71f3fddbcf6bf03d7a97169a540fd077c24815
sophie <sophiebucaille@gmail.com>
2016-05-01 14:46:47 +0200

    probleme 278 uva in java, Chess

---
+++ uva/Volume_002/P278.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class P278 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Integer casesNb = Integer.parseInt(sc.nextLine());
+
+		while(casesNb > 0) {
+			casesNb--;
+			char piece = sc.next(".{1}").charAt(0);
+			int m = sc.nextInt();
+			int n = sc.nextInt();
+			sc.nextLine();
+			
+			switch(piece) {
+			case 'r':
+				System.out.println(Math.min(n,m));
+				break;
+			case 'k':
+				System.out.println(((m + 1)/2) * ((n + 1)/2) + ((m/2) * (n/2)));
+				break;
+			case 'Q':
+				System.out.println(Math.min(n,m));
+				break;
+			case 'K':
+				System.out.println(((m + 1)/2) * ((n+1) /2));
+				break;
+			}
+
+		}
+
+	}
+
+}
+
