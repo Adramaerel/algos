1c3505e91da901c9c97407ff5543540e75379f7b
sophie <sophiebucaille@gmail.com>
2016-09-09 23:18:10 +0200

    uva problem 10110 in java, Light, more light

---
+++ uva/Volume_101/P10110.java
@@ -0,0 +1,21 @@
+package uva.p101;
+
+import java.util.Scanner;
+
+public class P10110 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String ligne = "";
+		while(!(ligne = sc.nextLine()).equals("0")) {
+			Long nb = Long.parseLong(ligne);
+			Long sq = (long) Math.sqrt(nb);
+			System.out.println(nb != sq * sq ? "no" : "yes");
+		}
+		sc.close();
+
+	}
+
+}
+
