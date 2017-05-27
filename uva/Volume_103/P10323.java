dd7cab5654368e430e7729f683521a6c98be2971
Adramaerel <adramaerel@gmail.com>
2017-05-28 00:24:11 +0200

    uva problem 10323 in java Factorial! You Must be Kidding!!!

--- uva/Volume_103/P10323.java
+++ uva/Volume_103/P10323.java
@@ -1,9 +1,14 @@
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * 
  */
 
 /**
- * @author Sophie
+ * @author Ethardolind
  *
  */
 public class P10323 {
@@ -12,8 +17,41 @@ public class P10323 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Map<Integer, BigInteger> map = new HashMap<>();
+
+		BigInteger previous = BigInteger.ONE;
+		map.put(new Integer(0), BigInteger.ONE);
+		map.put(new Integer(1), BigInteger.ONE);
+
+		for (int i = 2; i < 14; i++) {
+
+			map.put(new Integer(i), previous.multiply(new BigInteger(i + "")));
+			previous = previous.multiply(new BigInteger(i + ""));
+		}
+
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+
+			Integer i = Integer.parseInt(sc.nextLine());
+
+			if(i <= 0) {
+				if( i %2 == 0) {
+					System.out.println("Underflow!");
+				} else {
+					System.out.println("Overflow!");
+				}
+				
+			}
+			if (i > 0 && i < 8 ) {
+				System.out.println("Underflow!");
+			}
+			if( i >= 8 && i < 14) {
+				System.out.println(map.get(new Integer(i)));
+			}
+			if( i >= 14 ) {
+				System.out.println("Overflow!");
+			}
+		}
 
 	}
-
 }
