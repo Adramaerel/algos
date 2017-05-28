8677a417f72e85e232b12afd5d0b3e9b1e78cb75
Adramaerel <adramaerel@gmail.com>
2017-05-28 10:09:19 +0200

    UVA problem 11805 in Java : bafana Bafana

---
+++ uva/Volume_118/P11805.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+/**
+ * @author Ethardolind
+ *
+ */
+public class P11805 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Integer testCaseNb = Integer.parseInt(sc.nextLine());
+		for(int i = 0; i < testCaseNb;) {
+			String []tab = sc.nextLine().split(" ");
+			int nb = (Integer.parseInt(tab[2]) %  Integer.parseInt(tab[0]) + Integer.parseInt(tab[1])) % Integer.parseInt(tab[0]);
+			if(nb == 0) {
+				nb = Integer.parseInt(tab[0]);
+			}
+			System.out.printf("Case " + (++i) + ": %d\n", nb);
+		}
+	}
+
+}
