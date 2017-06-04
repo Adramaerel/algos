ba9f78be6d8ba5d57fd0bcb4acfbb1d5734a1d24
Adramaerel <adramaerel@gmail.com>
2017-06-04 11:16:38 +0200

    uva problem 748 in java Exponentiation

---
+++ uva/Volume_007/P748.java
@@ -0,0 +1,35 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * @author Ethardolind
+ *
+ */
+public class P748 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			String []tab = sc.nextLine().split(" ");
+			Double decimal1 = Double.parseDouble(tab[0]);
+			BigDecimal decimal = new BigDecimal(decimal1 + "");
+			Integer n = Integer.parseInt(tab[tab.length - 1]);
+			
+			String result = decimal.pow(n).toPlainString();
+			if(result.startsWith("0.")) {
+				result = result.substring(1, result.length());
+			}
+			System.out.println(result);
+
+
+
+			
+		}
+	}
+
+}
