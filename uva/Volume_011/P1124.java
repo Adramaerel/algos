f50b3d2758092289d2bffe15a91c4a57902f6765
Adramaerel <adramaerel@gmail.com>
2018-01-21 15:45:32 +0100

    uva probleme 1124 in java Celebrity jeopardy

---
+++ uva/Volume_011/P1124.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class P1124 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			
+			String ligne = sc.nextLine();
+			System.out.println(ligne);
+		}
+		sc.close();
+	}
+
+}
+
